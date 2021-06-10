f2f0a492c877803e09ba30122bae258db2f2a2d6
JanetZhuo <janetzhuo123@gmail.com>
2021-06-10 21:50:28 +1000

    weather command able to vaidate and print table of cities'weather out

--- app/Console/Commands/Weather.php
+++ app/Console/Commands/Weather.php
@@ -2,7 +2,11 @@
 
 namespace App\Console\Commands;
 
+use App\Http\Controllers\ApiController;
+use Carbon\Carbon;
 use Illuminate\Console\Command;
+use Illuminate\Http\Request;
+use Symfony\Component\Console\Helper\Table;
 
 class Weather extends Command
 {
@@ -38,6 +42,60 @@ class Weather extends Command
      */
     public function handle()
     {
-        //
+        $city = $this->argument('city');
+        $cityArr = explode(',',$city);
+
+        $request = new Request();
+        $header = [];
+        $tableData = [];
+
+        foreach ($cityArr as $k=>$v){
+            $request->offsetSet('city',$v);
+
+            //valiate if it's a valid city and if it's a city of Australia
+            $validateDataRes = (new ApiController())->currentWeather($request);
+            $validate = $validateDataRes->getData(true);
+            if($validate['status'] !== 1){
+                $this->line("<fg=red;bg=yellow>{$v}:{$validate['message']}</>");
+                continue;
+            }
+            if($validate['data']['country'] != 'AU') {
+                $this->line("<fg=red;bg=yellow>{$v} does not belong to the AU</>");
+                continue;
+            }
+
+            $res = (new ApiController())->weather($request);
+            $data = $res->getData(true);
+            if($data['status'] !== 1){
+                $this->line("<fg=red;bg=yellow>{$v}:{$data['message']}</>");
+                continue;
+            }
+            $weatherData = $data['data'];
+
+            //draw the table header for date
+            if(empty($header)){
+                $header[] = '';
+                foreach ($weatherData['list'] as $vv){
+                    $date = $vv[0]['dt_txt'];
+                    $header[] = Carbon::parse($date)->isoFormat('ddd MMM D');
+                }
+            }
+
+            //draw temprature for each city
+            $tempData = [];
+            $tempData[] = $v;
+            foreach ($weatherData['min_max'] as $vv){
+                $tempData[] = floor($vv['min']).'/'.floor($vv['max']).' °C';
+            }
+
+            $tableData[] = $tempData;
+        }
+
+        $table = new Table($this->output);
+        $table->setHeaders($header);
+        $table->setRows($tableData);
+        $table->render();
+
+        return;
     }
 }
